ef328f1b6377ee98e2082a04dbb50897c1cb38ad
cvs2git <>
2007-04-10 16:20:47 +0000

    This commit was manufactured by cvs2svn to create tag 'v0r2'.

--- cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/slac.stanford.edu/g/glast/applications/CMT/v1r16p20040701; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt build temporary_name -quiet`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt -quiet cleanup -sh -pack=GPLtools -version=v0 -path=/afs/slac.stanford.edu/u/ec/dragon/glast $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
