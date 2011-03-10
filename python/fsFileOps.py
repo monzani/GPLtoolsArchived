feb33a9dd7eb27cf04134695280924874bd9117e
focke <>
2011-03-10 21:20:05 +0000

    Merge changes from fileOps.

---
+++ python/fsFileOps.py
@@ -0,0 +1,124 @@
+"""Low-level file operations when all files involved are on a regular
+filesystem.
+"""
+
+import errno
+import os
+
+import cpck
+import runner
+
+## Set up message logging
+import logging
+log = logging.getLogger("gplLong")
+
+
+def copy(fromFile, toFile):
+    """
+    @brief copy a file
+    @param fromFile = name of ssource file
+    @param toFile = name of destination file
+    @return success code - actually always 0, raises exceptions on failure.
+
+    This just copies the file.
+    """
+    checksum = cpck.copyAndSum(fromFile, toFile)
+    log.info('Checksum = %s' % checksum)
+    return 0
+
+
+def exists(fileName):
+    rc = os.access(fileName, os.R_OK)
+    return rc
+
+
+def getSize(fileName):
+    try:
+        rc = os.stat(fileName).st_size
+    except OSError, e:
+        if e.errno != errno.ENOENT: raise
+        rc = None
+    return rc
+
+
+def makedirs(name, mode):
+    """makedirs(path,  mode)
+
+    Super-mkdir; create a leaf directory and all intermediate ones.
+    Works like mkdir, except that any intermediate path segment (not
+    just the rightmost) will be created if it does not exist.  This is
+    recursive.
+
+    This is a modified version of os.makedirs that's more careful to not
+    try to make directories that already exist. So it probably inherits the
+    Python2.5 license.
+
+    """
+    head, tail = os.path.split(name)
+    if not tail:
+        head, tail = os.path.split(head)
+    if head and tail and not os.path.exists(head):
+        try:
+            makedirs(head, mode)
+        except OSError, e:
+            # be happy if someone already created the path
+            if e.errno != errno.EEXIST: raise
+        if tail == os.curdir:     # xxx/newdir/. exists if xxx/newdir exists
+            return
+    if not os.path.exists(name):
+        try:
+            os.mkdir(name, mode)
+        except OSError, e:
+            # be happy if someone already created the path
+            if e.errno != errno.EEXIST: raise
+            pass
+        pass
+    return
+
+
+def mkdirFor(filePath, mode):
+    status = 0
+    dirPath = os.path.dirname(filePath)
+    if not os.path.isdir(dirPath):
+        log.info('Making directory %s' % dirPath)
+        makedirs(dirPath, mode)
+        pass
+    return status
+
+
+def remove(fileName):
+    try:
+        os.remove(fileName)
+        rc = 0
+    except OSError, e:
+        if e.errno != errno.ENOENT: raise
+        rc = 1
+    return rc
+
+
+def rmdir(name):
+    os.rmdir(name)
+    return 0
+
+
+def rmtree(name):
+    cmd = 'rm -rf %s' % name
+    rc = runner.run(cmd)
+    return rc
+
+
+def tempName(fileName):
+    tn = fileName + '.part'
+    return tn
+
+
+def unTemp(fileName):
+    tn = tempName(fileName)
+    log.info("Renaming %s to %s" % (tn, fileName))
+    rename(tn, fileName)
+    return 0
+
+
+def rename(src, dst):
+    os.rename(src, dst)
+    return 0
---
+++ python/xrootdFileOps.py
@@ -0,0 +1,91 @@
+"""Low-level file operations when at least on file is on xrootd.
+"""
+
+import os
+
+import runner
+
+xrootStart = "root:"
+xrootdLocation = os.getenv("GPL_XROOTD_DIR","/afs/slac.stanford.edu/g/glast/applications/xrootd/PROD/bin")
+xrdcp    = xrootdLocation+"/xrdcp "
+xrdstat  = xrootdLocation+"/xrd.pl -w stat "
+xrdrm    = xrootdLocation+"/xrd.pl rm "
+xrd      = xrootdLocation+"/xrd.pl"
+
+## Set up message logging
+import logging
+log = logging.getLogger("gplLong")
+
+
+def copy(fromFile, toFile):
+    """
+    @brief copy a staged file to final xrootd repository.
+    @param fromFile = name of staged file, toFile = name of final file
+    @return success code
+
+    This just copies the file.
+    """
+
+    xrdcmd=xrdcp+" -np -f "+fromFile+" "+toFile   #first time try standard copy
+    log.info("Executing...\n"+xrdcmd)
+    rc = runner.run(xrdcmd)
+    log.debug("xrdcp return code = "+str(rc))
+    
+    return rc
+
+
+def exists(fileName):
+    xrdcmd = xrdstat + fileName
+    xrdrc = runner.run(xrdcmd)
+    log.debug("xrdstat return code = " + str(xrdrc))
+    rc = not xrdrc
+    return rc
+
+
+def getSize(fileName):
+    xrdcmd = xrdstat + fileName
+    pipe = os.popen(xrdcmd)
+    lines = pipe.read()
+    rc = pipe.close()
+    if rc: return None
+    log.debug(lines)
+    size = int(lines.split()[1])
+    return size
+
+
+def makedirs(name, mode):
+    return 0
+
+
+def mkdirFor(fileName, mode):
+    return 0
+
+
+def remove(fileName):
+    xrdcmd = '%s rm %s' % (xrd, fileName)
+    rc = runner.run(xrdcmd)  ## failure is Okay => file does not already exist
+    return rc
+
+
+def rmdir(name):
+    return 0
+
+
+def rmtree(name):
+    xrdcmd = '%s rmtree %s' % (xrd, name)
+    rc = runner.run(xrdcmd)
+    return rc
+
+
+def tempName(fileName):
+    return fileName
+
+
+def unTemp(fileName):
+    return 0
+
+
+def rename(src, dst):
+    # Rename only works against a server, not the redirector.
+    # So if we want to implement this, we have to figure that out.
+    return 1
